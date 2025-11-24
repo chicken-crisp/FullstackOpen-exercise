c7b004edb5d5b3276d35154c829a2292dfc7c065
chicken-crisp <nfuuma0@gmail.com>
2025-11-25 06:06:24 +0900

    Changed something

--- part-1/courseinfo/part-1/src/components/Total.jsx
+++ part-1/courseinfo/part-1/src/components/Total.jsx
@@ -1,9 +1,8 @@
 const Total = ({arr}) => {
-    const sum = arr.reduce((a,b)=> a+b ,0)
 
     return (
     <p>
-        Number of exercises {sum}
+        Number of exercises {arr}
     </p>
     )
 }
